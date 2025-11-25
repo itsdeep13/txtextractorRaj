730969f00e11c685cae825048223630169e57ad8
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-11-25 08:04:00 +0530

    Modify BOT_TOKEN and ADMIN_ID in config.py
    Updated BOT_TOKEN and ADMIN_ID in the Config class.

--- config.py
+++ config.py
@@ -3,13 +3,14 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "8355123253:AAGJPTisze4F8NElZe1XCisyEeY7K6DiNFE"
+    BOT_TOKEN = "8509691849:AAGoOvZg1hQO4fE3OLUBD6eqNoDVGYJLgTc"
     DB_NAME = "uploaderbot"
     API_ID = "22484497"
     API_HASH = "c38cb053916c47a97590c244663cbaef"
-    ADMIN_ID = [6252997817, 6363345131, 7510415010]
+    ADMIN_ID = [6252997817]
     DB_URL = "mongodb+srv://uploaderbot:uploaderbot@cluster0.mpesxpw.mongodb.net/?retryWrites=true&w=majority"
-    LOG_CHANNEL = -1002927079315 # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    LOG_CHANNEL = -1003401547068 # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
     TUTORIAL_VIDEO = "https://t.me/TheRajBhaiOG"
+
 
